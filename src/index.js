ecd126699b2fad8e00562978ef0cf0b10ff0ce2f
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-08-23 16:34:01 -0300

    commit_connectTest

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json())
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/futbot', {
+mongoose.connect('mongodb://localhost:5500/createAcount.html', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
